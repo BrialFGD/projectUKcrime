fa9b3147b35b373282e36265fc83329beffcf899
Andrew Smith <andrewsmith7257@outlook.com>
2021-08-04 15:08:23 +0800

    cleaning branch

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import app_heat_mapping
 import app_time_mapping
 import app_plotting
 import app_home
+import app_predict
 
 PAGES = {"Home":app_home,
          "Crime HeatMaps":app_heat_mapping,
          "Crime TimeSeries Map":app_time_mapping,
-         "Crime Data":app_plotting
+         "Crime Data":app_plotting,
+         "Crime Outcome Prediction":app_predict
     
 }
 st.sidebar.title('UK Crime Tracker')
