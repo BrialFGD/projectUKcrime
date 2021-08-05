d21c4958e946a9935c06edc48de9896f589a6bc6
BrialFGD <brial.forestier@icloud.com>
2021-08-05 10:56:20 +0800

    finalize the predict.py and add preproc & model .pkl

--- app_predict.py
+++ app_predict.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import pickle
 
 def app():
     #Prepare user inputs to prediction model format
@@ -8,12 +9,19 @@ def app():
              st.session_state.crime_type]]
     X_df = pd.DataFrame(data, columns = ['lon', 'lat','crime'])
     
-    #load the fitted model pickle
-    model_filepath = 'raw_data/top_fitted.pkl'
-    outcome_model = pd.read_pickle(model_filepath)
+    #load the fitted preprocessor pickle
+    preproc_filepath = 'raw_data/full_fit_preproc.pkl'
+    preproc = pickle.load(open(preproc_filepath,'rb'))
+    
+    #preprocess X_df
+    X_preproc_df = preproc.transform(X_df)
+    
+    #Load the fitted XGBClassifier model
+    model_filepath = 'raw_data/big_fit_xgbc.pkl'
+    outcome_model = pickle.load(open(model_filepath,'rb'))
     
     #predict and present data
-    pred_proba = outcome_model.predict_proba(X_df)
+    pred_proba = outcome_model.predict_proba(X_preproc_df)
     predict_view = pd.DataFrame(zip(model.classes_,pred_proba[0]))
     predict_view.columns = ['outcome','probability']
     predict_view['probability'] = predict_view['probability'].apply(lambda x: round(x*100,2))
